89d91a3f6a3cc282c5b33094cceb103f9d4e75c3
sammiechum <147017220+sammiechum@users.noreply.github.com>
2023-10-05 23:05:46 -0400

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -1,65 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct  4 18:04:37 2023
+Created on Thu Oct  5 22:38:03 2023
 
 @author: sammiechum
 """
-
 import re
+import gzip
+import tarfile
+import os
 import string
-import sys
-# import gzip
-# import zipfile
-
-# with zipfile.ZipFile('/Users/sammiechum/Downloads/prez_speeches.zip', 'r') as prez_zip:
-#     # Loop through the files in the zip folder
-#     for prez_gz in prez_zip.infolist():
-#         speech_text = prez_zip.read(prez_gz.filename)
-
-# print(speech_text)
-
 library = []
+
 def clean_text(text):
-    text = text.lower()
+    text = str(text).lower()
     text = re.sub('\[.*?\]', '', text)
     text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
     text = re.sub('[\d\n]', ' ', text)
     return text
 
-with open('/Users/sammiechum/Downloads//AFINN.txt', 'r') as file:
+with open('/AFINN.txt', 'r') as file:
     for line in file:
         words = line.split()
         dict = {
             "word": words[0],
             "value": words[1] }
         library.append(dict)
-        
-for line in sys.stdin:
-    cleaned = clean_text(line)
-# cleaned = "i like you so much"
 
-words = cleaned.split()
-for word in words:
-    for i in range (len(library)):
-        if (library[i]["word"] == word):
-            key = word
-            value = library[i]["value"]
-            print(key,"\t",value)
+def determineValence(content):
+    for line in content:
+        cleaned = clean_text(line)
+        words = cleaned.split()
+        for word in words:
+            for i in range (len(library)):
+                if (library[i]["word"] == word):
+                    key = word
+                    value = library[i]["value"]
+                    print(key,"\t",value)
+                    
+for gz_file in os.listdir('/prez_speeches'):
+    pathtofile = os.path.join('/prez_speeches',gz_file)
+    
+    # print(pathtofile)
+    with gzip.open(pathtofile, 'rb') as gz_file:
+    # Read the contents of the compressed file
+        # print(gz_file)
+        with tarfile.open(fileobj= gz_file, mode='r') as tar:
+        # List the contents of the tar archive (optional)
+            # print(tar.list())
+            for txt in tar.getmembers():
+                speech = tar.extractfile(txt)
+                if speech:
+                    content = speech.read()
+                    determineValence(content)
+                    # print(content.decode('utf-8'))
 
-
-
-# J's sample mapper
-# def main(argv):
-#     line = sys.stdin.readline()
-#     pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
-#     try:
-#         while line:
-#             for word in pattern.findall(line):
-#                 print ("LongValueSum:" + word.lower() + "\t" + "1")
-#             line = sys.stdin.readline()
-#     except EOFError as error:
-#         return None
-
-# if __name__ == "__main__":
-#     main(sys.argv)
