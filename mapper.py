e48e5967ef2afa365b0a433e0b18fa474780ad93
sammiechum <147017220+sammiechum@users.noreply.github.com>
2023-10-05 23:24:16 -0400

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -5,7 +5,7 @@ Created on Thu Oct  5 22:38:03 2023
 
 @author: sammiechum
 """
-import re
+import sys, re
 import gzip
 import tarfile
 import os
@@ -19,13 +19,6 @@ def clean_text(text):
     text = re.sub('[\d\n]', ' ', text)
     return text
 
-with open('/AFINN.txt', 'r') as file:
-    for line in file:
-        words = line.split()
-        dict = {
-            "word": words[0],
-            "value": words[1] }
-        library.append(dict)
 
 def determineValence(content):
     for line in content:
@@ -37,21 +30,33 @@ def determineValence(content):
                     key = word
                     value = library[i]["value"]
                     print(key,"\t",value)
-                    
-for gz_file in os.listdir('/prez_speeches'):
-    pathtofile = os.path.join('/prez_speeches',gz_file)
-    
-    # print(pathtofile)
-    with gzip.open(pathtofile, 'rb') as gz_file:
-    # Read the contents of the compressed file
-        # print(gz_file)
-        with tarfile.open(fileobj= gz_file, mode='r') as tar:
-        # List the contents of the tar archive (optional)
-            # print(tar.list())
-            for txt in tar.getmembers():
-                speech = tar.extractfile(txt)
-                if speech:
-                    content = speech.read()
-                    determineValence(content)
-                    # print(content.decode('utf-8'))
 
+def main(argv):
+    # line = sys.stdin.readline()
+    with open(argv[1], 'r') as file:
+        for line in file:
+            words = line.split()
+            dict = {
+                "word": words[0],
+                "value": words[1] }
+            library.append(dict)
+            
+    for gz_file in os.listdir(argv[0]):
+        pathtofile = os.path.join(argv[0],gz_file)
+        
+        # print(pathtofile)
+        with gzip.open(pathtofile, 'rb') as gz_file:
+        # Read the contents of the compressed file
+            # print(gz_file)
+            with tarfile.open(fileobj= gz_file, mode='r') as tar:
+            # List the contents of the tar archive (optional)
+                # print(tar.list())
+                for txt in tar.getmembers():
+                    speech = tar.extractfile(txt)
+                    if speech:
+                        content = speech.read()
+                        determineValence(content)
+                        # print(content.decode('utf-8'))
+
+if __name__ == "__main__":
+    main(sys.argv)
